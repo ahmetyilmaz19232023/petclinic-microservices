5f6e7141f4139943803447122a84371c16adc5c3
ahmetyilmaz19232023 <ahmetyilmaz19232023@gmail.com>
2020-12-27 13:26:46 +0000

    added scripts for qa automation environment

--- jenkins/jenkins-petclinic-ci-job.sh
+++ jenkins/jenkins-petclinic-ci-job.sh
@@ -1,2 +1,2 @@
 echo 'Running Unit Tests on Petclinic Application'
-docker run --rm -v /var/lib/jenkins/.m2:/root/.m2 -v /var/lib/jenkins/workspace/testmsp-17:/app -w /app maven:3.6-openjdk-11 mvn clean package
+docker run --rm -v $HOME/.m2:/root/.m2 -v $WORKSPACE:/app -w /app maven:3.6-openjdk-11 mvn clean package
