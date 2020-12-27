0bc21d629bf47b475f14910370a76498358bd5e2
ahmetyilmaz19232023 <ahmetyilmaz19232023@gmail.com>
2020-12-27 14:07:38 +0000

    added qa automation pipeline for dev

--- jenkins/jenkins-petclinic-ci-job.sh
+++ jenkins/jenkins-petclinic-ci-job.sh
@@ -1,1 +1,2 @@
-docker run --rm -v /var/lib/jenkins/.m2:/root/.m2 -v /var/lib/jenkins/workspace/testmsp-17:/app -w ./app maven:3.6-openjdk-11 mvn clean package
+echo 'Running Unit Tests on Petclinic Application'
+docker run --rm -v $HOME/.m2:/root/.m2 -v `pwd`:/app -w /app maven:3.6-openjdk-11 mvn clean test
